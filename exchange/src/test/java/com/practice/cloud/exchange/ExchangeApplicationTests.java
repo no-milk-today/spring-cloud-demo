bc371ddeff84c352507ae21443d02f20506f4b46
no-milk-today <drmhere@yandex.by>
2025-06-18 10:21:09 +0300

    #exchange initial

---
+++ exchange/src/test/java/com/practice/cloud/exchange/ExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.cloud.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

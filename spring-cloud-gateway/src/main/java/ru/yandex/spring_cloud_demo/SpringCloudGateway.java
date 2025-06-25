7764726e5d211038a187af1d740101a5b4ac1f7f
no-milk-today <drmhere@yandex.by>
2025-06-25 12:08:29 +0300

    #initial from theory

---
+++ spring-cloud-gateway/src/main/java/ru/yandex/spring_cloud_demo/SpringCloudGateway.java
@@ -0,0 +1,13 @@
+package ru.yandex.spring_cloud_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGateway.class, args);
+    }
+
+}
